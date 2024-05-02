4e0a62668230203df6ffcb76574bcc07a0790a80
Daniel Holler <danablend@gmail.com>
2024-05-02 13:56:43 +0100

    Update FramerAuthLib.js

--- FramerAuthLib.js
+++ FramerAuthLib.js
@@ -1,34 +1,34 @@
 var _a;
 /**
- * FramerAuthFormType is an enum that represents the different types of forms that can be used on FramerAuth sites.
+ * FramerAuthComponentType is an enum that represents the different types of component that can be used on FramerAuth sites.
  */
-var FramerAuthFormType;
-(function (FramerAuthFormType) {
-    FramerAuthFormType["SIGN_IN"] = "sign_in";
-    FramerAuthFormType["SIGN_UP"] = "sign_up";
-    FramerAuthFormType["CHANGE_PASSWORD"] = "change_password";
-    FramerAuthFormType["FORGOT_PASSWORD"] = "forgot_password";
-    FramerAuthFormType["ACTIVATE_LICENSE_KEY"] = "activate_license";
-    FramerAuthFormType["MEMBER_PROFILE"] = "member_profile";
-})(FramerAuthFormType || (FramerAuthFormType = {}));
+var FramerAuthComponentType;
+(function (FramerAuthComponentType) {
+    FramerAuthComponentType["SIGN_IN"] = "sign_in";
+    FramerAuthComponentType["SIGN_UP"] = "sign_up";
+    FramerAuthComponentType["CHANGE_PASSWORD"] = "change_password";
+    FramerAuthComponentType["FORGOT_PASSWORD"] = "forgot_password";
+    FramerAuthComponentType["ACTIVATE_LICENSE_KEY"] = "activate_license";
+    FramerAuthComponentType["MEMBER_PROFILE"] = "member_profile";
+})(FramerAuthComponentType || (FramerAuthComponentType = {}));
 /**
- * FramerAuthForm is a class that represents a form that can be used on FramerAuth sites.
+ * FramerAuthComponent is a class that represents a form that can be used on FramerAuth sites.
  */
-var FramerAuthForm = /** @class */ (function () {
-    function FramerAuthForm(_name, _type, _moduleUrl) {
+var FramerAuthComponent = /** @class */ (function () {
+    function FramerAuthComponent(_name, _type, _moduleUrl) {
         this.name = _name;
         this.type = _type;
         this.moduleUrl = _moduleUrl;
     }
-    return FramerAuthForm;
+    return FramerAuthComponent;
 }());
-var framerAuthForms = (_a = {},
-    _a[FramerAuthFormType.SIGN_IN] = new FramerAuthForm("Sign In", FramerAuthFormType.SIGN_IN, 'https://framer.com/m/AuthSignInForm-6WfL.js@TikE7jtU4v6p0h56OPPB'),
-    _a[FramerAuthFormType.SIGN_UP] = new FramerAuthForm("Sign Up", FramerAuthFormType.SIGN_UP, 'https://framer.com/m/AuthSignUpForm-3LGT.js@8LqkPACmOTDOn2rd2CiV'),
-    _a[FramerAuthFormType.CHANGE_PASSWORD] = new FramerAuthForm("Change Password", FramerAuthFormType.CHANGE_PASSWORD, 'https://framer.com/m/AuthChangePassword-jjQ2.js@csqAaCtzfuu3wl7744Lf'),
-    _a[FramerAuthFormType.FORGOT_PASSWORD] = new FramerAuthForm("Forgot Password", FramerAuthFormType.FORGOT_PASSWORD, 'https://framer.com/m/AuthForgotPasswordForm-qpKO.js@0x0iYh2z50YACi9Pg8Cb'),
-    _a[FramerAuthFormType.ACTIVATE_LICENSE_KEY] = new FramerAuthForm("Activate License Key", FramerAuthFormType.ACTIVATE_LICENSE_KEY, 'https://framer.com/m/AuthActivateLicenseKey-Ej0S.js@MsuNk7lbUFvUfnayL2Bl'),
-    _a[FramerAuthFormType.MEMBER_PROFILE] = new FramerAuthForm("Member Profile", FramerAuthFormType.MEMBER_PROFILE, 'https://framer.com/m/AuthMemberProfile-ioi1.js@xXrpzSc2tXLe4q0CEdGN'),
+var framerAuthComponents = (_a = {},
+    _a[FramerAuthComponentType.SIGN_IN] = new FramerAuthComponent("Sign In", FramerAuthComponentType.SIGN_IN, 'https://framer.com/m/AuthSignInComponent-6WfL.js@TikE7jtU4v6p0h56OPPB'),
+    _a[FramerAuthComponentType.SIGN_UP] = new FramerAuthComponent("Sign Up", FramerAuthComponentType.SIGN_UP, 'https://framer.com/m/AuthSignUpComponent-3LGT.js@8LqkPACmOTDOn2rd2CiV'),
+    _a[FramerAuthComponentType.CHANGE_PASSWORD] = new FramerAuthComponent("Change Password", FramerAuthComponentType.CHANGE_PASSWORD, 'https://framer.com/m/AuthChangePassword-jjQ2.js@csqAaCtzfuu3wl7744Lf'),
+    _a[FramerAuthComponentType.FORGOT_PASSWORD] = new FramerAuthComponent("Forgot Password", FramerAuthComponentType.FORGOT_PASSWORD, 'https://framer.com/m/AuthForgotPasswordComponent-qpKO.js@0x0iYh2z50YACi9Pg8Cb'),
+    _a[FramerAuthComponentType.ACTIVATE_LICENSE_KEY] = new FramerAuthComponent("Activate License Key", FramerAuthComponentType.ACTIVATE_LICENSE_KEY, 'https://framer.com/m/AuthActivateLicenseKey-Ej0S.js@MsuNk7lbUFvUfnayL2Bl'),
+    _a[FramerAuthComponentType.MEMBER_PROFILE] = new FramerAuthComponent("Member Profile", FramerAuthComponentType.MEMBER_PROFILE, 'https://framer.com/m/AuthMemberProfile-ioi1.js@xXrpzSc2tXLe4q0CEdGN'),
     _a);
 /**
  * FramerAuthLib contains useful constants and functions for building modular components for the FramerAuth plugin.
@@ -37,9 +37,9 @@ var FramerAuthLib = /** @class */ (function () {
     function FramerAuthLib() {
     }
     /**
-     * Record of FramerAuthForm objects that represent the different types of forms that can be used on FramerAuth sites.
+     * Record of FramerAuthComponent objects that represent the different types of component that can be used on FramerAuth sites.
      */
-    FramerAuthLib.FORMS = framerAuthForms;
+    FramerAuthLib.COMPONENTS = framerAuthComponents;
     return FramerAuthLib;
 }());
-export default FramerAuthLib;
+export { FramerAuthComponentType, FramerAuthComponent, FramerAuthLib };
